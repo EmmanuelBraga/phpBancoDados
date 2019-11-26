4f9d55a23e1b16ea4ee70f975ed7b991fc6fd12c
Técnico em Informática - 2019.1 <169121742019.1@senacrjedu.local>
2019-11-26 20:51:11 -0200

    novos arquivos

--- pages/add-endereco.php
+++ pages/add-endereco.php
@@ -1,30 +1,34 @@
-<form action="">
-<div class="form-group">
-    <label for="cep">CEP</label>
-    <input type="text" class="form-control" id="cep" name="cep">
-</div>
-<div class="form-group">
-    <label for="logradouro">Endereço</label>
-    <input type="text" class="form-control" id="logradouro" name="logradouro">
-</div>
-<div class="form-group">
-    <label for="bairro">Bairro</label>
-    <input type="text" class="form-control" id="bairro" name="bairro">
-</div>
-<div class="form-group">
-    <label for="cidade">Cidade</label>
-    <input type="text" class="form-control" id="cidade" name="cidade">
-</div>
-<div class="form-group">
-    <label for="estado">Estado</label>
-    <input type="text" class="form-control" id="uf" name="uf">
-</div>
-<button type="submit" class="btn btn-primary">Salvar</button>
-<button type="reset" class="btn btn-primary">Cancelar</button>
-</form>
+<div class="container">
+    <form method="POST">
+        <div class="form-group">
+            <label for="cep">CEP</label>
+            <input type="text" class="form-control" id="cep" name="cep" onblur="pesquisacep(this.value)" maxlength="9" require>
+        </div>
+        <div class="form-group">
+            <label for="logradouro">Endereço</label>
+            <input type="text" class="form-control" id="logradouro" name="logradouro" require>
+        </div>
+        <div class="form-group">
+            <label for="bairro">Bairro</label>
+            <input type="text" class="form-control" id="bairro" name="bairro" require>
+        </div>
+        <div class="form-group">
+            <label for="cidade">Cidade</label>
+            <input type="text" class="form-control" id="cidade" name="cidade" require>
+        </div>
 
+        <div class="form-group">
+            <label for="uf">Estado</label>
+            <input type="text" class="form-control" id="uf" name="uf" maxlength="2" require>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Salvar</button>
+        <button type="reset" class="btn btn-primary">Cancelar</button>
+    </form>
+</div>
+<script src="js/viacep.js"></script>
 <?php
-if(!empty($_POST)) {
+if (!empty($_POST)) {
     require_once("./model/endereco.php");
     $endereco = new Endereco;
     $endereco->cep = $_POST['cep'];
